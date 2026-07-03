9fa1ee12565d52e97ea4120db4d37c897ba5cd99
felipefvega <datfiliv@gmail.com>
2026-07-03 11:34:23 -0400

    docs: agrega anotaciones swagger a controllers

--- src/main/java/cl/unaccess/notificaciones/controller/NotificacionController.java
+++ src/main/java/cl/unaccess/notificaciones/controller/NotificacionController.java
@@ -3,6 +3,12 @@ package cl.unaccess.notificaciones.controller;
 import cl.unaccess.notificaciones.model.Notificacion;
 import cl.unaccess.notificaciones.dto.NotificacionDTO;
 import cl.unaccess.notificaciones.service.NotificacionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/notificaciones")
+@Tag(name = "Notificaciones", description = "Gestion de notificaciones a pacientes")
 public class NotificacionController {
 
     private final NotificacionService service;
@@ -18,13 +25,18 @@ public class NotificacionController {
         this.service = service;
     }
 
-   
+    @Operation(summary = "Enviar una notificacion a un paciente")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Notificacion enviada",
+            content = @Content(examples = @ExampleObject(value = "{\"pacienteRut\":\"12345678-9\",\"mensaje\":\"Recordatorio de cita\",\"tipoCanal\":\"sms\"}")))
+    })
     @PostMapping
     public ResponseEntity<Notificacion> crear(@RequestBody NotificacionDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.enviarNotificacion(dto));
     }
 
-    
+    @Operation(summary = "Listar notificaciones de un paciente por su RUT")
+    @ApiResponse(responseCode = "200", description = "Lista de notificaciones del paciente")
     @GetMapping("/paciente/{rut}")
     public ResponseEntity<List<Notificacion>> listarPorPaciente(@PathVariable String rut) {
         return ResponseEntity.ok(service.listarPorPaciente(rut));
